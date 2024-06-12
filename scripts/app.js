773f61a9d1672321b69e7327b53f6cc889cb495e
Christina Nkya <mrs.nkya@gmail.com>
2024-06-11 22:53:14 -0400

    106 assignment 2

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,13 @@
-function sayHello() {
-    console.log("Hello World")//displays the message in the console//This is the child of sayHello function
+function saveTask(){//save the task to the local storage
+    console.log("Task Saved");//display task saved in the console
 }
-function sayGoodbye() {
-    console.log("boy bye")//displays the message in the console//This is the child of sayGoodbye function
+function init(){//initialize the application
+    console.log("task manager");
+//load data
+//hook the events
+$("#btnSave").click(saveTask);
 }
-function init() {//this is the child of winddow.onload
-    console.log("Whats up world")//displays the message in the console//this is the child of init function
-    sayHello();//calls the function//this is the child of the init function
-    sayGoodbye();//calls the function//this is the child of the init function
-}
+//function getTask{//get the task from the local storage
+
+//}
 window.onload = init;//calls the init function when the page is loaded// This is the parent
