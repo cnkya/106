c2f66d20984b8e7e2022714600288c3f1f0ce2a6
Christina Nkya <mrs.nkya@gmail.com>
2024-06-13 22:47:51 -0400

    106 assignment 3

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,54 @@
 function saveTask(){//save the task to the local storage
     console.log("Task Saved");//display task saved in the console
+    
+    //get the values from the html
+    const title=$("#txtTitle").val();
+    const description=$("#txtDescription").val();
+    const color=$("#selColor").val();
+    const startdate=$("#selStartDate").val();
+    const status=$("#selStatus").val();
+    const budget=$("#numBudget").val();
+    console.log(title, description, color,startdate, status, budget)
+
+    //build an object
+    let taskToSave = new Task(title, description, color,startdate, status, budget); //build task object
+    console.log(taskToSave);
+    // Save to the server
+
+    //display the info from the server
+    displayTask(taskToSave);
+//try to create a function to clear at the moment we hit the save button
+    clearForm(taskToSave); 
+
+}
+
+function displayTask(task){
+
+    let syntax = 
+    `<div class ='task'>
+        <div class='info'>
+            <h3>Title: ${task.title}</h3>
+            <h5>Description: ${task.description}</h5>
+        </div>
+            <h5 class="status">Status: ${task.status}</h5>
+        <div class="date-budget">
+            <h5>Date: ${task.startdate}</h5>
+            <h5>Budget: ${task.budget}</h5>
+        </div>
+    </div>
+    
+    `
+
+    $(".list-task").append(syntax);
+    
+}
+function clearForm(){// clear the form after you hit save button
+    $("#txtTitle").val("");
+    $("#txtDescription").val("");
+    $("#selColor").val("");
+    $("#selStartDate").val("");
+    $("#selStatus").val("");
+    $("#numBudget").val("");
 }
 function init(){//initialize the application
     console.log("task manager");
@@ -7,7 +56,14 @@ function init(){//initialize the application
 //hook the events
 $("#btnSave").click(saveTask);
 }
-//function getTask{//get the task from the local storage
 
+
+
+
+
+
+
+
+//function getTask{//get the task from the local storage
 //}
 window.onload = init;//calls the init function when the page is loaded// This is the parent
---
+++ scripts/task.js
@@ -0,0 +1,12 @@
+class Task{//new library that contains the constructor
+
+    constructor(title, descript, color, startdate, status, budget){//using a constructor to list the attributes
+        this.title = title;//title of the task
+        this.description = descript;//description of the task
+        this.color = color;//color of the task
+        this.startdate = startdate;//start date
+        this.status = status;// status of the task
+        this.budget = budget;//budget of the task
+        this.name = "Christina";
+    }
+}
